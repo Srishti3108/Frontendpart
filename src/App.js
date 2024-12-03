e4750019cfe84493eaa95c3f8334bd2d6115a595
Srishti3108 <138809318+Srishti3108@users.noreply.github.com>
2024-12-03 21:35:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import SecuredComponent from "./components/SecuredComponent";
+
+// Regular Dashboard Components
+import RegularDashboard from "./components/RegularDashboard/RegularSidebar";
+import RegularHome from "./components/RegularDashboard/RegularHome";
+import AssetCatalog from "./components/RegularDashboard/AssetCatalog";
+import BorrowedAssets from "./components/RegularDashboard/BorrowedAssets";
+
+// Admin Dashboard Components
+import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
+import AdminHome from "./components/AdminDashboard/AdminHome";
+import EmployeeManagement from "./components/AdminDashboard/EmployeeManagement";
+import AssetManagement from "./components/AdminDashboard/AssetManagement";
+import AssetRequests from "./components/AdminDashboard/AssetRequest";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Redirect '/' to '/login' */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Login and Register Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Secured Component Route */}
+        <Route path="/secured" element={<SecuredComponent />} />
+
+        {/* Regular Dashboard Routes */}
+        <Route path="/regular/*" element={<RegularDashboard />}>
+          <Route path="home" element={<RegularHome />} />
+          <Route path="assets" element={<AssetCatalog />} />
+          <Route path="borrowed" element={<BorrowedAssets />} />
+        </Route>
+
+        {/* Admin Dashboard Routes */}
+        <Route path="/admin/*" element={<AdminDashboard />}>
+          <Route path="home" element={<AdminHome />} />
+          <Route path="employees" element={<EmployeeManagement />} />
+          <Route path="assets" element={<AssetManagement />} />
+          <Route path="requests" element={<AssetRequests />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
